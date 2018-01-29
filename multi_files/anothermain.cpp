a666a8601aa5f70439ff278e1c9259519de31f4b
Mike Zamansky <zamansky@gmail.com>
2018-01-29 10:57:43 -0500

    added multi files

---
+++ multi_files/anothermain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "circle_funcs.h"
+
+int main()
+{
+  std::cout << "This main just has a lot of code to test everything\n";
+  std::cout << circ(50) << std::endl;
+
+  return 0;
+}
---
+++ multi_files/circle_funcs.cpp
@@ -0,0 +1,7 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+
+double circ(double r){
+  return 2*M_PI*r;
+}
